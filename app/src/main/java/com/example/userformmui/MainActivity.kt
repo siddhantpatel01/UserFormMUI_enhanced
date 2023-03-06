dabf361146ac5122da87ad3ace40d05d8188bb8d
Siddhant Patel <siddhantpatel445@gmail.com>
2023-03-06 13:21:19 +0530

    UsingIntent

--- app/src/main/java/com/example/userformmui/MainActivity.kt
+++ app/src/main/java/com/example/userformmui/MainActivity.kt
@@ -1,17 +1,169 @@
 package com.example.userformmui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.renderscript.ScriptGroup.Binding
-import android.view.inputmethod.InputBinding
+import android.view.View
+import android.widget.CompoundButton
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.userformmui.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+class MainActivity : AppCompatActivity(), View.OnClickListener, RadioGroup.OnCheckedChangeListener,
+    CompoundButton.OnCheckedChangeListener {
+    private lateinit var binding: ActivityMainBinding
+    val list: ArrayList<String> = ArrayList()
+    var gender: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // This peace of code for Submit Button
+        binding.submit.setOnClickListener(this)
+
+        // This peace of code for Radio Button
+        binding.radioButton.setOnCheckedChangeListener(this)
+
+        // This peace of code for Check Box
+        binding.Coding.setOnCheckedChangeListener(this)
+        binding.readingBook.setOnCheckedChangeListener(this)
+        binding.movies.setOnCheckedChangeListener(this)
+        binding.Playing.setOnCheckedChangeListener(this)
+        binding.traveling.setOnCheckedChangeListener(this)
+    }
+
+    override fun onClick(view: View) {
+
+
+        when (view?.id) {
+
+            R.id.submit -> {
+
+                if (binding.Firstname.text!!.isEmpty() && binding.Lastname.text!!.isEmpty() && binding.Phone1.editableText.isEmpty() && binding.Phone2.editableText.isEmpty() && binding.email.text!!.isEmpty()) {
+                    binding.Firstname.requestFocus()
+                    Toast.makeText(this, "fill required information ", Toast.LENGTH_SHORT).show()
+                } else if (binding.Firstname.text!!.isEmpty()) {
+                    binding.Firstname.requestFocus()
+                    Toast.makeText(this, "Please fill your First name", Toast.LENGTH_SHORT).show()
+
+                } else if (binding.Lastname.text!!.isEmpty()) {
+                    binding.Lastname.requestFocus()
+                    Toast.makeText(this, "Please fill your Last name", Toast.LENGTH_SHORT).show()
+
+                } else if (binding.Phone1.editableText.isEmpty()) {
+                    binding.Phone1.requestFocus()
+                    Toast.makeText(this, "Please fill your Phone Number name", Toast.LENGTH_SHORT)
+                        .show()
+                } else if (binding.Phone2.editableText.isEmpty()) {
+                    binding.Phone2.requestFocus()
+                    Toast.makeText(
+                        this,
+                        "Please fill your Alternate Number name",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else if (binding.email.text!!.isEmpty()) {
+                    binding.email.requestFocus()
+                    Toast.makeText(this, "Please fill your Email name", Toast.LENGTH_SHORT).show()
+
+                } else if (binding.Firstname.text!!.isNotEmpty() && binding.Lastname.text!!.isNotEmpty() && binding.Phone1.editableText.isNotEmpty() && binding.Phone2.editableText.isNotEmpty() && binding.email.text!!.isNotEmpty()) {
+
+                    val intent = Intent(this, UserInformation::class.java)
+                    val firstname = binding.Firstname.editableText.toString()
+                    val lastname = binding.Lastname.editableText.toString()
+                    val phone1 = binding.Phone1.editableText.toString()
+                    val phone2 = binding.Phone2.editableText.toString()
+                    val email = binding.email.editableText.toString()
+                    val lst=list.toString().replace("["," ").replace("]"," ")
+
+
+
+                    intent.putExtra("firstname", firstname)
+                    intent.putExtra("lastname", lastname)
+                    intent.putExtra("phone1", phone1)
+                    intent.putExtra("phone2", phone2)
+                    intent.putExtra("email", email)
+                    intent.putExtra("gender", gender)
+                    intent.putExtra("hobbies",lst)
+
+                    startActivity(intent)
+
+
+                }
+
+
+            }
+        }
+
+    }
+
+    override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
+        when (group?.checkedRadioButtonId) {
+            R.id.male -> {
+                val rbMale = findViewById<RadioButton>(checkedId)
+                gender = rbMale.text.toString()
+            }
+            R.id.Female -> {
+                val rbFemale = findViewById<RadioButton>(checkedId)
+                gender = rbFemale.text.toString()
+            }
+            R.id.TransGender -> {
+                val rbtransgender = findViewById<RadioButton>(checkedId)
+                gender = rbtransgender.text.toString()
+            }
+
+        }
+    }
+
+    override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+        when (buttonView?.id) {
+            R.id.Coding -> {
+                if (binding.Coding.isChecked) {
+                    list.add(binding.Coding.text.toString())
+                    // list.toString().replace("[", "").replace("]", "");
+                } else {
+                    list.remove(binding.Coding.text.toString())
+                }
+
+            }
+            R.id.readingBook -> {
+                if (binding.readingBook.isChecked) {
+                    list.add(binding.readingBook.text.toString())
+
+                } else {
+                    list.remove(binding.readingBook.text.toString())
+                }
+
+            }
+            R.id.movies -> {
+                if (binding.movies.isChecked) {
+                    list.add(binding.movies.text.toString())
+                    // list.toString().replace("[", "").replace("]", "");
+                } else {
+                    list.remove(binding.movies.text.toString())
+                }
+
+            }
+            R.id.Playing -> {
+                if (binding.Playing.isChecked) {
+                    list.add(binding.Playing.text.toString())
+                    // list.toString().replace("[", "").replace("]", "");
+                } else {
+                    list.remove(binding.Playing.text.toString())
+                }
+
+            }
+            R.id.traveling -> {
+                if (binding.traveling.isChecked) {
+                    list.add(binding.traveling.text.toString())
+                    // list.toString().replace("[", "").replace("]", "");
+                } else {
+                    list.remove(binding.traveling.text.toString())
+                }
+
+            }
+        }
     }
 }
--- app/src/main/java/com/example/userformmui/UserInformation.kt
+++ app/src/main/java/com/example/userformmui/UserInformation.kt
@@ -1,11 +1,31 @@
 package com.example.userformmui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.userformmui.databinding.ActivityUserInformationBinding
 
 class UserInformation : AppCompatActivity() {
+    private lateinit var binding: ActivityUserInformationBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_information)
+        binding = ActivityUserInformationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//
+//        val intent = intent
+//        val  name = intent.getStringExtra("name")
+//        binding.info.editText.toString()=""
+        val firstname = intent.getStringExtra("firstname")
+        val lastname = intent.getStringExtra("lastname")
+        val phone1 = intent.getStringExtra("phone1")
+        val phone2 = intent.getStringExtra("phone2")
+        val email = intent.getStringExtra("email")
+        val gender= intent.getStringExtra("gender")
+        val hobbies= intent.getStringExtra("hobbies")
+        binding.info.text = " Name:-  $firstname $lastname \n Phone No:-  $phone1 \n Alternative Phone No :- $phone2  \n Email :- $email  \n Gender :-  $gender \n Hobbies :- $hobbies"
+
+
     }
 }
